4189acf83e9c9c1b5ad4814328d523c4c2340974
Ömer Emir Zıvalı <116160889+omerzval@users.noreply.github.com>
2025-03-18 17:14:52 +0300

    Add files via upload

---
+++ HumanController.cs
@@ -0,0 +1,33 @@
+﻿using Basics.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+	public class HumanController : Controller
+	{
+		public IActionResult Index()	
+		{
+			var insan = new Human();
+			insan.Id = 5;
+			insan.Name = "Ömer Emir Zıvalı";
+
+			return View(insan);
+		}
+		public IActionResult List()
+		{
+			var insanlar = new List<Human>()
+			{
+				new Human(){Id=1,Name="Ahmet Yaşar"},
+				new Human(){Id=2,Name="Mehmet Yaşar"},
+				new Human(){Id=3,Name="Veli Yaşar"},
+				new Human(){Id=4,Name="Samet Yaşar"},
+
+
+			};
+
+			return View(insanlar);
+		}
+		
+	}
+}
